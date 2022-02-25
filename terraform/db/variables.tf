bdd9dd745f9893faceb380e6cc9fffe3bbc69417
Anand Dyavanapalli <adyavanapalli@gmail.com>
2022-02-25 15:44:27 -0500

    refactor: Remove unused variable.

--- terraform/db/variables.tf
+++ terraform/db/variables.tf
@@ -1,9 +1,3 @@
-# variable "public_key" {
-#   description = "The public key which should be used for authentication, which needs to be at least 2048-bit and in ssh-rsa format."
-#   sensitive   = true
-#   type        = string
-# }
-
 variable "region" {
   default     = "East US"
   description = "The Azure Region where the Resource Group should exist."
